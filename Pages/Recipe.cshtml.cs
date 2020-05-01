bfc41534a26e0aa00d25477bae16fcda30e6729f
Andreas Lundgren <alundgren@users.noreply.github.com>
2020-05-01 07:06:43 +0200

    initial

---
+++ Pages/Recipe.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace irudd_cooking.Pages
+{
+    public class RecipeModel : PageModel
+    {
+        private readonly ILogger<RecipeModel> _logger;
+
+        public RecipeModel(ILogger<RecipeModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
